ab51dda921e5fa46d9f68f568215b420c25aa895
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-03-11 15:26:25 +0530

    Added NavBar

--- app/src/main/java/bk/acs/MainActivity.java
+++ app/src/main/java/bk/acs/MainActivity.java
@@ -1,6 +1,7 @@
 package bk.acs;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,27 +19,22 @@ import bk.acs.RecyclerView.ListItem;
 import bk.acs.RecyclerView.MyAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    FloatingActionButton fab;
     RecyclerView recview;
     MyAdapter adapter;
     Data d;
     Toolbar toolbar;
+    NavigationDrawer drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        fab=(FloatingActionButton)findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MyDialog dialog=new MyDialog();
-                dialog.show(getFragmentManager(),"myDialog");
-            }
-        });
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
         d=new Data(this);
         recview=(RecyclerView)findViewById(R.id.recview);
+        drawer=(NavigationDrawer)getSupportFragmentManager().findFragmentById(R.id.navDrawer);
+        drawer.setUp((DrawerLayout)findViewById(R.id.drawer_layout),toolbar);
         adapter=new MyAdapter(d.getList(),this);
         recview.setLayoutManager(new LinearLayoutManager(recview.getContext()));
         recview.setAdapter(adapter);
@@ -56,7 +52,8 @@ public class MainActivity extends AppCompatActivity {
         int res_id=item.getItemId();
         if(res_id==R.id.refresh)
         {
-            adapter.notifyDataSetChanged();
+            MyDialog dialog=new MyDialog();
+            dialog.show(getFragmentManager(),"myDialog");
         }
         return true;
     }
--- app/src/main/java/bk/acs/NavigationDrawer.java
+++ app/src/main/java/bk/acs/NavigationDrawer.java
@@ -3,6 +3,9 @@ package bk.acs;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +16,10 @@ import android.view.ViewGroup;
  */
 public class NavigationDrawer extends Fragment {
 
-
+    ActionBarDrawerToggle drawertoggle;
+    DrawerLayout drawerlayout;
+    boolean userLearnedDrawer;
+    boolean FromSavedInstanceState;
     public NavigationDrawer() {
         // Required empty public constructor
     }
@@ -26,4 +32,27 @@ public class NavigationDrawer extends Fragment {
         return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
     }
 
+    public void setUp(DrawerLayout drawerlayout, Toolbar toolbar) {
+        this.drawerlayout=drawerlayout;
+        drawertoggle=new ActionBarDrawerToggle(getActivity(),drawerlayout,toolbar,R.string.drawer_open,R.string.drawer_close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+        };
+        drawerlayout.setDrawerListener(drawertoggle);
+        drawerlayout.post(new Runnable() {
+            @Override
+            public void run() {
+                drawertoggle.syncState();
+            }
+        });
+    }
 }
